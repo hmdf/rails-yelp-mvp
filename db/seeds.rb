002a0f7e36066f119fc216b1b61eef77a91c5342
hortense martin <hortense.mdf@gmail.com>
2017-11-22 18:51:52 +0100

    Creating reviews and restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "chinese"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "italian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "belgian"
+  },
+  {
+    name:         "Bocuse",
+    address:      "Collonges",
+    category:     "japanese"
+  },
+  {
+    name:         "Liu Bo Lin",
+    address:      "french",
+    category:     "italian"
+  }
+]
+Restaurant.create!(restaurants_attributes)
